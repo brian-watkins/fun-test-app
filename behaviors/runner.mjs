d608926bb7e33307e4d4c34d0dea5b93a64ac8e6
Brian Watkins <bwatkins@vmware.com>
2022-11-06 15:06:55 -0500

    Tests start browser and load html

--- behaviors/runner.mjs
+++ behaviors/runner.mjs
@@ -1,6 +1,29 @@
+import { createServer } from "vite"
+import { chromium } from "playwright"
 import { validate } from "esbehavior";
 import funBehavior from "./fun.behavior"
+
+const server = await createServer({
+    server: {
+        port: 5957
+    }
+})
+
+await server.listen()
+
+const browser = await chromium.launch({
+    headless: false
+})
+
+const page = await browser.newPage()
+page.on("console", console.log)
+page.on("pageerror", console.log)
+
+await page.goto("http://localhost:5957/behaviors/index.html")
 
 await validate([
     funBehavior
 ])
+
+// await browser.close()
+// await server.close()
