4fba44233311d235a94c1e1216e576e8ea3a8c83
Brian Watkins <bwatkins@vmware.com>
2022-11-10 06:28:41 -0600

    Add debug mode to tests
    - so that it’s easy to make the browser
    visible in certain cases

--- behaviors/runner.mjs
+++ behaviors/runner.mjs
@@ -3,27 +3,45 @@ import { chromium } from "playwright"
 import { validate } from "esbehavior";
 import funBehavior from "./fun.behavior"
 
+// Start Vite
 const server = await createServer({
-    server: {
-        port: 5957
-    }
+  server: {
+    port: 5957
+  }
 })
 
 await server.listen()
 
+
+// Start the browser
 const browser = await chromium.launch({
-    headless: true
+  headless: !isDebug()
 })
 
 const page = await browser.newPage()
 page.on("console", console.log)
 page.on("pageerror", console.log)
 
+
+// Load the Test Display
 await page.goto("http://localhost:5957/behaviors/index.html")
 
+
+// Run the tests
 await validate([
-    funBehavior(page)
+  funBehavior(page)
 ])
 
-await browser.close()
-await server.close()
+
+// Close
+if (!isDebug()) {
+  await browser.close()
+  await server.close()
+}
+
+
+// Helper functions -->
+
+function isDebug() {
+  return process.env["DEBUG"] !== undefined
+}
